085130fba1c1a3951a79fc99ab0673be3406a256
wuyan1993 <2012.Wuyan>
2016-03-01 19:17:58 +0800

    wuyan

---
+++ iosProgram/iosTest/ios22/ios22/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios22
+//
+//  Created by lkjy on 16/3/1.
+//  Copyright © 2016年 lkjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosProgram/iosTest/ios22/ios22/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios22
+//
+//  Created by lkjy on 16/3/1.
+//  Copyright © 2016年 lkjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
